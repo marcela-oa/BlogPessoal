67e25f18b6d980c99b77d8689d0710f00f9eca07
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-03-24 12:19:56 -0300

    CRUD parte 01

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,59 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens") // a anotação não pode ter espaço
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo título deve possuir no mínimo 5 e no máximo 100 caracteres.")
+	private String titulo;
+	
+	@NotNull(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve possuir no mínimo 10 e no máximo 1000 caracteres.")
+	private String texto;
+	
+	@UpdateTimestamp //atualiza a hora na hora da criação da postagem e toda vez que fizer modificações
+	private LocalDate data;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+}
